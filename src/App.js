7a4df3fc96e244fb3e82404aed9d0d487183bfb9
Chang <cnxiong03@gmail.com>
2020-04-22 00:23:12 -0400

    created mock resource file and dynically load resources to app

--- src/App.js
+++ src/App.js
@@ -1,33 +1,27 @@
 import React, { Component } from 'react';
 import './App.css';
 import Resource from './components/Resource'
+import resources from './mock/resources'
 
 class App extends Component {
 
-  resource = {
-    posterName: 'Chang',
-    resourceAuthor: 'NetNinja',
-    authorSkillLevel: 'Advanced',
-    Cohort: '8',
-    title: 'React & Redux Complete',
-    Category: ['React', 'Redux'],
-    Summary: 'A good resource on building a couple react apps with and without redux',
-    Link: 'www.google.com/search?q=NetNinja%20React',
-    resourceType: 'Playlist',
-    datePublished: '2018-09-10.00:00:00.000z',
-    videoLength: null,
-    timeToComplete: 300,
-    rating: 5,
-    comments: [{user: 'Cole', text:'Pretty good resource'}, {user: 'Kendrick', text: 'Helped me a lot learning React.'}]
-}
+  // constructor(props) {
+  //   super(props);
+  //   this.renderPosts.bind(this)
+  // }
+
+  renderPosts() {
+    const display = resources.map((resource) => {
+      return <Resource resource={resource} />;
+    })
+    return display;
+  }
 
   render () {
     return (
       <div className="App">
         <h1>Welcome to Brainhive2</h1>
-        <Resource resource={this.resource} />
-        <Resource resource={this.resource} />
-        <Resource resource={this.resource} />
+        {this.renderPosts()}
       </div>
     );
   }
---
+++ src/mock/resources.js
@@ -0,0 +1,61 @@
+const resources = [
+    {
+      posterName: 'Chang',
+      resourceAuthor: 'NetNinja',
+      authorSkillLevel: 'Advanced',
+      Cohort: '8',
+      title: 'React & Redux Complete',
+      Category: ['React', 'Redux'],
+      Summary: 'A good resource on building a couple react apps with and without redux',
+      Link: 'www.google.com/search?q=NetNinja%20React',
+      resourceType: 'Playlist',
+      datePublished: '2018-09-10.00:00:00.000z',
+      videoLength: null,
+      timeToComplete: 300,
+      rating: 5,
+      comments: [
+        {user: 'Cole', text:'Pretty good resource'}, 
+        {user: 'Kendrick', text: 'Helped me a lot learning React.'}
+      ]
+    },
+    {
+      posterName: "Chang",
+      resourceAuthor: "Web Dev Simplified",
+      authorSkillLevel: "Advanced",
+      cohort: "0",
+      title: "Build A REST API with Node.js, Express, & MongoDB",
+      categories: ["Node.js", "Express", "MongoDB"],
+      summary: "A good resource on building a Rest API with Node.js, Express, & MongoDB",
+      link: "https://www.youtube.com/watch?v=fgTGADljAeg",
+      resourceType: "Video",
+      datePublished: "2019-05-14.00:00:00.000z",
+      videoLength: null,
+      timeToComplete: 30,
+      rating: 5,
+      comments: [
+        { user: "Chang", text: "Quick and easy to learn MEN stack" },
+        { user: "Smith", text: "Great video!" },
+      ],
+    },
+    {
+      posterName: "Joel",
+      resourceAuthor: "Traversy Media",
+      authorSkillLevel: "Advance",
+      cohort: "8",
+      title: "React JS Crash Crourse",
+      categories: ["React"],
+      summary: "Code along about React on youtube",
+      link: "https://www.youtube.com/watch?v=sBws8MSXN7A",
+      resourceType: "Crash Course",
+      datePublished: "2018-09-10.00:00:00.000z",
+      videoLength: null,
+      timeToComplete: 180,
+      rating: 5,
+      comments: [
+        { user: "john", text: "Great video with clear instructions" },
+        { user: "Bella", text: "Simple and straight to the point." },
+      ],
+    },
+  ];
+
+  export default resources;
